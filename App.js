7aad5cd31ce36bdfb58cd479a2b2d1b906b42289
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-12-06 15:29:39 -0600

    Added Chat screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 // import the screens
 import Start from './components/Start';
-//import Chat from './components/Chat';
+import Chat from './components/Chat';
 
 // import react native gesture handler 
 import 'react-native-gesture-handler';
@@ -21,6 +21,8 @@ export default class App extends Component {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Start">
           <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+
         </Stack.Navigator>
       </NavigationContainer>
     );
---
+++ components/Chat.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default class Chat extends React.Component {
+  constructor() {
+    super();
+  }
+
+
+  componentDidMount(){
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+  }
+
+  render() {
+    let color=this.props.route.params.color;
+    return (
+      <View style={[{flex: 1}, {backgroundColor: color}]}>
+
+      </View>
+    )
+  }
+}
