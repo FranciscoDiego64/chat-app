eb317980235eff887fe0724d96a861ca272309a3
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-12-07 14:57:22 -0600

    Added Gifted chat to chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,97 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
   constructor() {
     super();
+    this.state = {
+      messages: [],
+    }
   }
 
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  //change bubble colour
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#5f9ea0'
+          }
+        }}
+      />
+    )
+  }
 
   componentDidMount(){
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'You have entered the chat.',
+          createdAt: new Date(),
+          system: true,
+         },
+
+        {
+          _id: 2,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+
   }
 
   render() {
     let color=this.props.route.params.color;
     return (
       <View style={[{flex: 1}, {backgroundColor: color}]}>
-        
-
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+          _id: 1
+          }}
+          accessible={true}
+          accessibilityLabel='Chat input field'
+          accessibilityHint='Here you can enter the message. After entering the message, you can press send on the right.'
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
 }
+
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 40,     
+  },
+  item: {
+    fontSize: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 30,
+  }
+});
+*/
